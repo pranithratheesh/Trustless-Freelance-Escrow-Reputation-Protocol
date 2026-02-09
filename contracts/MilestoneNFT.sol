5e7ea671e2a90e3837d6461a5faae6ab8dcb0105
Pranith Ratheesh <pranithratheesh@gmail.com>
2026-02-09 15:16:29 +0530

    Update MilestoneNFT.sol

--- contracts/MilestoneNFT.sol
+++ contracts/MilestoneNFT.sol
@@ -5,11 +5,19 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MilestoneNFT is ERC1155, Ownable {
+    address public escrow;
+
+    mapping(uint256 => address) public milestoneOwner;
+
+    modifier onlyEscrow() {
+    require(msg.sender == escrow);
+    _;
+}
     uint256 public nextMilestoneId;
 
     constructor() ERC1155("") Ownable(msg.sender) {}
 
-    function createMilestone(address client) external onlyOwner returns (uint256) {
+    function createMilestone(address client) external onlyEscrow returns (uint256) {
         uint256 id = nextMilestoneId++;
         _mint(client, id, 1, "");
         return id;
